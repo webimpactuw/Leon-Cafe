8faaf8fc80803bd8f449d911f186502b5b670dd1
Anna <wanganna@uw.edu>
2023-04-06 17:00:57 -0700

    Continue working on nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import Gallery from './pages/Gallery';
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Routes>{/* <Route path="/" exact element={}/> */}</Routes>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/gallery' element={<Gallery />} />
+      </Routes>
     </>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,13 +4,33 @@ import '../styles/NavBar.css';
 const NavBar = () => {
   return (
     <nav>
-      <ul>
-        <li>Home</li>
-        <li></li>
-        <li></li>
-        <li></li>
+      <img src='/img/logo.svg' alt='Leon Coffee House Seattle home' />
+      <ul className='nav-links'>
+        <li>
+          <NavLink to='/' className='link'>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/menu' className='link'>
+            Menu
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/gallery' className='link'>
+            Gallery
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/about' className='link'>
+            About
+          </NavLink>
+        </li>
       </ul>
-      <div className='social-icons'></div>
+      <div className='social-icons'>
+        <img src='/img/facebook.svg' alt='Facebook' />
+        <img src='/img/instagram.svg' alt='Instagram' />
+      </div>
     </nav>
   );
 };
