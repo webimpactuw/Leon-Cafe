156e06f05c124ceb42e8ab8febe76b25055e0d9a
Anna <wanganna@uw.edu>
2023-04-11 18:30:33 -0700

    Make nav bar responsive, add social media links to icons, continue styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,32 +4,51 @@ import '../styles/NavBar.css';
 const NavBar = () => {
   return (
     <nav>
-      <img src='/img/logo.svg' alt='Leon Coffee House Seattle home' />
-      <ul className='nav-links'>
-        <li>
-          <NavLink to='/' className='link'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/menu' className='link'>
-            Menu
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/gallery' className='link'>
-            Gallery
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/about' className='link'>
-            About
-          </NavLink>
-        </li>
-      </ul>
-      <div className='social-icons'>
-        <img src='/img/facebook.svg' alt='Facebook' />
-        <img src='/img/instagram.svg' alt='Instagram' />
+      <div className='nav-container'>
+        <img
+          src='/img/logo.svg'
+          alt='Leon Coffee House Seattle home'
+          width='96'
+          height='96'
+        />
+        <ul className='nav-links'>
+          <li>
+            <NavLink to='/' className='link'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/menu' className='link'>
+              Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/gallery' className='link'>
+              Gallery
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/about' className='link'>
+              About
+            </NavLink>
+          </li>
+        </ul>
+        <div className='social-icons'>
+          <a
+            href='https://www.facebook.com/leoncoffeehouseseattle'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src='/img/facebook.svg' alt='Facebook' />
+          </a>
+          <a
+            href='https://www.instagram.com/leoncoffeehouse/?hl=en'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src='/img/instagram.svg' alt='Instagram' />
+          </a>
+        </div>
       </div>
     </nav>
   );
