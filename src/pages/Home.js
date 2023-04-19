6b18d4df2706d04e4718a39e97fa95671468557c
mia2520 <mia2520@uw.edu>
2023-04-18 17:11:03 -0700

    Added HTML elements to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,22 @@
 const Home = () => {
 
     return(
-      <>
-      home.
-      </>
+      <div>
+        <header>
+          <h1> Welcome To Leon Coffee House</h1>
+          <h2> Family owned Mexican coffee house</h2>
+        </header>
+
+        <main>
+          <h3>About</h3>
+          <p>Leon Coffee House is a cozy and welcoming cafe in the heart of Seattle. 
+             We serve freshly brewed coffee, savory waffles, and delicious light bites in 
+             a relaxed and friendly atmosphere. Our mission is to provide a comfortable
+             space for our community to gather, connect, and enjoy good food and drinks. </p>
+          
+          <h3>Menu</h3>
+        </main>
+      </div>
     )
 }
   
