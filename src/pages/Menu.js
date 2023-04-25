0421700f43b0750895c973f024a3ae67da5521c4
mia2520 <mia2520@uw.edu>
2023-04-24 23:17:49 -0700

    set up menu page

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,7 +2,24 @@ const Menu = () => {
 
     return(
       <>
-      menu.
+        <main>
+          <section className="menu__headers">
+            <p> Drinks </p>
+            <p> Sandwiches </p>
+            <p> Waffles </p>
+          </section>
+
+
+          <section className="menu__download">
+            <p> Download PDF </p>
+            <img src="/img/downloadIcon.svg" />
+          </section>
+
+          <section className="menu__image">
+            <img src="/img/drinksMenu1.jpg"/>
+            <img src="/img/drinksMenu2.jpg"/>
+          </section>
+        </main>
       </>
     )
 }
