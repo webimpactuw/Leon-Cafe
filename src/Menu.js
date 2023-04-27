187f234309a7d7e95ae68cc3212ca9657d1f0ec6
mia2520 <mia2520@uw.edu>
2023-04-27 15:02:48 -0700

    still need to work on the highlight color

---
+++ src/Menu.js
@@ -0,0 +1,1 @@
+const drink = document.querySelector("drink")
--- src/pages/drinksMenu.js
+++ src/pages/drinksMenu.js
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
+import { useState } from "react"; 
 import '../styles/Menu.css';
 
 const DrinksMenu = () => {
+  const [background, setBackground] = useState('transparent');
+  const handleClick = () => {
+    setBackground(prevBackground => prevBackground === 'transparent' ? 'pink' : 'transparent');
+  }
 
     return(
       <>
         <main className="menu__main">
           <section className="menu__headers">
             <ul>
-              <li><Link to="/drinksmenu"><span>Drinks</span>|</Link></li>
+              <li onClick={handleClick} style={{backgroundColor: background}}><Link to="/drinksmenu"><span>Drinks</span>|</Link></li>
               <li><Link to="/sandwichesmenu"><span>Sandwiches</span>|</Link></li>
               <li><Link to="/wafflesmenu"><span>Waffles</span></Link></li>
             </ul>
